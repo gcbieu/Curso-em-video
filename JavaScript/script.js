c2b8a9ba348977cda8d134f16e6e6cd0e3e00b0f
Gabriel Oliveira <gabrielcosta862@gmail.com>
2026-09-07 19:28:28 -0300

    Aula dia 7/09/2026

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+// Variáveis servem para guardar dados */
+/* 
+IDENTIFICADORES
+Podem começar com letras, $ ou _
+Não podem começar com números
+É possivel usar letras ou números
+É possivel usar acentos e símbolos
+Não podem conter espaços
+Não podem ser palavras reservadas
+
+// DICAS
+
+MAÍUCULAS e minúsculas fazem diferença
+Tente escolher nomes coerentes para as variáveis
+*/
