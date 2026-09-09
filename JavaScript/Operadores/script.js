40802be2df0df225c97d519baab5235910b159b6
Gabriel Oliveira <gabrielcosta862@gmail.com>
2026-09-09 18:53:11 -0300

    Aula 09/09/2026

---
+++ JavaScript/Operadores/script.js
@@ -0,0 +1,61 @@
+/*
+Operadores aritméticos
+5 + 2 -> 7
+5 - 2 -> 3
+5 * 2 -> 10
+5 / 2 -> 2.5
+5 % 2 -> 1
+5 ** 2 -> 25 
+
+Ordem de precedência
+
+()
+**
+*
+* / %
++ - 
+
+var a = 5 + 3
+var b = a % 5
+var c = 5 * b ** 2
+var d = 10 - a/2
+var e = 6 = 2 / s
+var f = b % e + 4/ e
+
+auto-atribuição
+var n = 3
+n = n + 4
+n = n - 5
+n = n * 4
+n = n / 2
+n = n ** 2
+n = n % 5
+
+simplificando
+n += 4
+n -= 5
+n *= 4
+n /= 2
+n **= 2
+n %= 2
+
+Incremento
+var x = 5
+x = x + 1 x++
+x = x - 1 x--
+
+Relacionais
+5 > 2 = true
+7 < 4 = false
+8 >= 8 = true
+9 <= 7 = false
+5 == 5 = true
+4 != 4 = false
+
+Identidade
+5 == 5 = true
+5 == '5' = true
+5 == '5' = false
+5 ===
+*/
+
