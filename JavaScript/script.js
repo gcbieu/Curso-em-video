8b83a6f135a095c7afe0b11801faf1c0d3e932a8
Gabriel Oliveira <gabrielcosta862@gmail.com>
2026-09-14 22:46:02 -0300

    Aula 14/09/2026

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,3 +13,69 @@ Não podem ser palavras reservadas
 MAÍUCULAS e minúsculas fazem diferença
 Tente escolher nomes coerentes para as variáveis
 */
+
+// DOM
+// Document Object Model
+/*
+ÁRVORE DOM
+
+window
+├── location
+├── document
+│   └── html
+│       ├── head
+│       │   ├── meta
+│       │   └── title
+│       │
+│       └── body
+│           ├── h1
+│           ├── p
+│           ├── p
+│           │   └── strong
+│           └── div
+│
+└── history
+
+// Selecionando
+/*
+SELECIONANDO ELEMENTOS NO DOM
+
+Por marca (tag)
+→ getElementsByTagName()
+
+Por ID
+→ getElementById()
+
+Por nome
+→ getElementsByName()
+
+Por classe
+→ getElementsByClassName()
+
+Por seletor
+→ querySelector()
+→ querySelectorAll()
+
+// EVENTOS DOM
+
+/*
+Movimentos com o mouse:
+
+mouseenter
+mousemove
+mousedown
+mouseup
+click
+mouseout
+
+
+FUNÇÕES
+
+Conjunto de linhas que vão ser executadas
+quando o evento ocorrer.
+
+function ação(parâmetros) {
+    // bloco
+}
+
+*/
