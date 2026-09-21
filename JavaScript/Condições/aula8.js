ef751af248a946c56aeb500adf2fc59abda096bd
Gabriel Oliveira <gabrielcosta862@gmail.com>
2026-09-21 19:31:37 -0300

    Aula 21/09/2026

---
+++ JavaScript/Condições/aula8.js
@@ -0,0 +1,9 @@
+const vel = 12
+
+console.log (`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultapassou a velocidade permitida. MULTADO`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
